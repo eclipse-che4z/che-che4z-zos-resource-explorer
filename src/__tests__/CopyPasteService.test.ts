6174ecfd34e05169fac5c3d434a226bc0239ba4c
az672136 <andrea.zaccaro@broadcom.com>
2019-08-29 17:48:00 +0200

    code refactoring
    Signed-off-by: az672136 <andrea.zaccaro@broadcom.com>

--- src/__tests__/CopyPasteService.test.ts
+++ src/__tests__/CopyPasteService.test.ts
@@ -11,52 +11,31 @@
  * Contributors:
  *   Broadcom, Inc. - initial API and implementation
  */
-
 jest.mock("../service/ZoweRestClient");
 jest.mock("../service/DatasetService");
 jest.mock("../service/CredentialsService");
 
 import { Connection } from "../model/Connection";
-import { Dataset, Member } from "../model/DSEntities";
+import { Member } from "../model/DSEntities";
 import { CopyPasteService } from "../service/CopyPasteService";
 import { DatasetService } from "../service/DatasetService";
 import { ZoweRestClient } from "../service/ZoweRestClient";
 import { ZDatasetNode, ZMemberNode } from "../ui/tree/DatasetTreeModel";
-
-// DOUBLE CHECK IF IS POSSIBLE DEFINE THE SERVICES GLOBALLY..
+import { createDummyDataset } from "../utils";
 
 describe("Copy member operations", () => {
+    const dataset = createDummyDataset();
+    const host: Connection = { name: "", url: "", username: "" };
+
+    // create separate method that fullfill these objects
+    const creds: any = {};
+    const restStub = new ZoweRestClient(creds);
+    const datasetServiceMocked: DatasetService = new DatasetService(restStub);
+
+    const pathPrefix: string = "PathPrefix";
+
     test("[POSITIVE TEST] Copy operation is available on member node", () => {
-        // use a stub member node
-        const creds: any = {};
-        const restStub = new ZoweRestClient(creds);
-
-        const host: Connection = { name: "", url: "", username: "" };
         const memberName: Member = { name: "Member1" };
-        const pathPrefix: string = "Pre";
-
-        const dataset: Dataset = {
-            allocatedSize: 15,
-            allocationUnit: "BLOCK",
-            averageBlock: 0,
-            blockSize: 6160,
-            catalogName: "ICFCAT.MV3B.CATALOGA",
-            creationDate: "2017/07/25",
-            dataSetOrganization: "PO",
-            deviceType: "3390",
-            directoryBlocks: 10,
-            expirationDate: "2020/07/25",
-            migrated: false,
-            name: "STEVENH.DEMO.JCL",
-            primary: 10,
-            recordFormat: "FB",
-            recordLength: 80,
-            secondary: 5,
-            used: 0,
-            volumeSerial: "3BP001",
-        };
-
-        const datasetServiceMocked: DatasetService = new DatasetService(restStub);
         const treeMemberNode: ZMemberNode = new ZMemberNode(dataset, memberName, host, pathPrefix);
         const copyPasteService: CopyPasteService = new CopyPasteService(restStub, datasetServiceMocked);
 
@@ -64,36 +43,6 @@ describe("Copy member operations", () => {
     });
 
     test("[NEGATIVE TEST] Copy operation is not available on dataset node", () => {
-        // TODO remove code duplication
-        // use a stub member node
-        const creds: any = {};
-        const restStub = new ZoweRestClient(creds);
-
-        const host: Connection = { name: "", url: "", username: "" };
-        const pathPrefix: string = "Pre";
-
-        const dataset: Dataset = {
-            allocatedSize: 15,
-            allocationUnit: "BLOCK",
-            averageBlock: 0,
-            blockSize: 6160,
-            catalogName: "ICFCAT.MV3B.CATALOGA",
-            creationDate: "2017/07/25",
-            dataSetOrganization: "PO",
-            deviceType: "3390",
-            directoryBlocks: 10,
-            expirationDate: "2020/07/25",
-            migrated: false,
-            name: "TEST.DATASET",
-            primary: 10,
-            recordFormat: "FB",
-            recordLength: 80,
-            secondary: 5,
-            used: 0,
-            volumeSerial: "3BP001",
-        };
-
-        const datasetServiceMocked: DatasetService = new DatasetService(restStub);
         const treeDatasetNode: ZDatasetNode = new ZDatasetNode(dataset, host, pathPrefix);
         const copyPasteService: CopyPasteService = new CopyPasteService(restStub, datasetServiceMocked);
 
@@ -101,34 +50,8 @@ describe("Copy member operations", () => {
     });
 
     test("[POSITIVE TEST] Member content is stored internally ready for paste operation", () => {
-        const host: Connection = { name: "", url: "", username: "" };
         const datasetName: string = "TEST.DATASET";
         const memberName: string = "MEM1";
-
-        const creds: any = {};
-        const restStub = new ZoweRestClient(creds);
-
-        const dataset: Dataset = {
-            allocatedSize: 15,
-            allocationUnit: "BLOCK",
-            averageBlock: 0,
-            blockSize: 6160,
-            catalogName: "ICFCAT.MV3B.CATALOGA",
-            creationDate: "2017/07/25",
-            dataSetOrganization: "PO",
-            deviceType: "3390",
-            directoryBlocks: 10,
-            expirationDate: "2020/07/25",
-            migrated: false,
-            name: "TEST.DATASET",
-            primary: 10,
-            recordFormat: "FB",
-            recordLength: 80,
-            secondary: 5,
-            used: 0,
-            volumeSerial: "3BP001",
-        };
-        const datasetServiceMocked: DatasetService = new DatasetService(restStub);
         const copyPasteService: CopyPasteService = new CopyPasteService(restStub, datasetServiceMocked);
 
         copyPasteService.copy(host, datasetName, memberName).then(() => {
